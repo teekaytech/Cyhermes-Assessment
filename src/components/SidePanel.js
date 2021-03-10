9166f703ce92b6388f6d5cbf3934fe9ab699484e
teekaytech <taofeek.olalere@feghas.com>
2021-03-10 11:39:42 +0100

    Style sidebar nav items.

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,5 +1,30 @@
 import '../stylesheets/sidepanel.scss';
+import Logo from '../images/logo.png';
 
-const SidePanel = () => (<p>SidePanel</p>);
+const SidePanel = () => (
+  <div className="SideBar">
+    <figure className="SideBar__logo">
+      <img src={Logo} alt="page-logo" className="SideBar__logo--length" />
+    </figure>
+    <nav>
+      <ul className="SideBar__nav">
+        <li className="SideBar__nav__item">
+          <a href="#pricing">
+            Trial & Pricing
+            {' '}
+            <span className="activeItem">&#10137;</span>
+          </a>
+        </li>
+        <li className="SideBar__nav__item">
+          <a href="#pricing">
+            Payment Method
+            {' '}
+            <span className="activeItem">&#10137;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
 
 export default SidePanel;
