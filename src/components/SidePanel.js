028459d1e688fe02c0f352585f7a24d58890b319
teekaytech <taofeek.olalere@feghas.com>
2021-03-10 13:24:04 +0100

    Complete adding and styling of sidebar component

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,29 +1,48 @@
 import '../stylesheets/sidepanel.scss';
 import Logo from '../images/logo.png';
+import Quote from '../images/quote.png';
 
 const SidePanel = () => (
   <div className="SideBar">
-    <figure className="SideBar__logo">
-      <img src={Logo} alt="page-logo" className="SideBar__logo--length" />
-    </figure>
-    <nav>
-      <ul className="SideBar__nav">
-        <li className="SideBar__nav__item">
-          <a href="#pricing">
-            Trial & Pricing
-            {' '}
-            <span className="activeItem">&#10137;</span>
+    <div>
+      <figure className="SideBar__logo">
+        <img src={Logo} alt="page-logo" className="SideBar__logo--length" />
+      </figure>
+      <nav>
+        <ul className="SideBar__nav">
+          <li className="SideBar__nav__item">
+            <a href="#pricing">
+              Trial & Pricing
+              {' '}
+              <span className="activeItem">&#10132;</span>
+            </a>
+          </li>
+          <li className="SideBar__nav__item">
+            <a href="#payment">
+              Payment Method
+              {' '}
+              <span className="activeItem">&#10132;</span>
+            </a>
+          </li>
+        </ul>
+        <div className="gettingStarted">
+          <span className="dotted">---------</span>
+          <a href="#started" className="start">
+            Get started...
           </a>
-        </li>
-        <li className="SideBar__nav__item">
-          <a href="#pricing">
-            Payment Method
-            {' '}
-            <span className="activeItem">&#10137;</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+        </div>
+      </nav>
+    </div>
+    <div className="SideBar__quote">
+      <figure className="SideBar__quote--symbol">
+        <img src={Quote} alt="page-logo" className="quoteImg" />
+      </figure>
+      <p className="SideBar__quote--text">
+        Success is not final; failure is not fatal: it is courage to continue
+        that counts.
+      </p>
+      <span className="SideBar__quote--author">-Winston Churchill</span>
+    </div>
   </div>
 );
 
