0d777b78bea2918b3b59b502e54171c2065cad63
teekaytech <taofeek.olalere@feghas.com>
2021-03-10 15:51:59 +0100

    Add and style contents in Subscribe card section

--- src/components/SubscribeCard.js
+++ src/components/SubscribeCard.js
@@ -1,3 +1,46 @@
-const SubscribeCard = () => ('Subscribe');
+import '../stylesheets/subscribe.scss';
+
+const SubscribeCard = () => (
+  <section className="Subscribe">
+    <article className="Start">
+      <p className="Content">
+        <span className="Content__init">Starts From</span>
+        <span className="Content__currency">NGN</span>
+        <span className="Content__amount">5,999,999,99</span>
+        per annum
+      </p>
+      <p className="Current">
+        <span className="Current__plan">Premium</span>
+        100 Users
+        {' '}
+        <span className="Current__dot">&#9679;</span>
+        {' '}
+        All Modules
+      </p>
+    </article>
+    <article className="Plan">
+      <div className="Plan__mark">✔</div>
+      <h6 className="Plan__header">Basic</h6>
+      <p className="Plan__more">
+        <span className="Plan__days">30 days trails</span>
+        {' '}
+        on our premium app
+        features and features
+      </p>
+
+      <p>
+        INCLUDES
+        {' '}
+        <span>--------------------</span>
+      </p>
+      <ul className="Plan__plackage">
+        <li className="Plan__package--items">25 Users</li>
+        <li className="Plan__package--items">All modules & features</li>
+        <li className="Plan__package--items">Unlimited Commitees</li>
+        <li className="Plan__package--items">Unlimited roles creation</li>
+      </ul>
+    </article>
+  </section>
+);
 
 export default SubscribeCard;
--- src/components/containers/Main.js
+++ src/components/containers/Main.js
@@ -2,14 +2,13 @@ import Header from '../Header';
 import Module from '../Module';
 import SubscribeCard from '../SubscribeCard';
 import UserBar from '../UserBar';
+import '../../stylesheets/main.scss';
 
 const Main = () => (
   <>
     <Header />
     <main>
-      <section>
-        <SubscribeCard />
-      </section>
+      <SubscribeCard />
       <section>
         <UserBar />
       </section>
